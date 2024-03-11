0a29eaed9ef96d2df15b3ad854f4120c996520d8
saeed joulaei <saeedjoulaei86@gmail.com>
2024-03-11 21:01:37 +0330

    card component created and styled

--- src/pages/ProductsPages.jsx
+++ src/pages/ProductsPages.jsx
@@ -1,9 +1,22 @@
+import Card from "../components/Card";
 import { useProducts } from "../context/ProductContext";
+
+import styles from "../pages/ProductsPages.module.css";
 
 function ProductsPages() {
   const products = useProducts();
-  console.log(products);
-  return <div>ProductsPages</div>;
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.products}>
+        {!products.length && <p>Loading...</p>}
+        {products.map((p) => (
+          <Card key={p.id} data={p} />
+        ))}
+      </div>
+      <div>sidebar</div>
+    </div>
+  );
 }
 
 export default ProductsPages;
