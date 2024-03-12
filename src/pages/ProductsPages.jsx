38a4cd3bbc2e4327345e7ae76aed915f5f8c3b3e
saeed joulaei <saeedjoulaei86@gmail.com>
2024-03-12 15:09:57 +0330

    search field created and added

--- src/pages/ProductsPages.jsx
+++ src/pages/ProductsPages.jsx
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import { ImSearch } from "react-icons/im";
+
 import Card from "../components/Card";
 import Loader from "../components/Loader";
 import { useProducts } from "../context/ProductContext";
@@ -6,18 +9,36 @@ import styles from "../pages/ProductsPages.module.css";
 
 function ProductsPages() {
   const products = useProducts();
+  const [search, setSearch] = useState("");
+
+  const searchHandler = () => {
+    console.log("click");
+  };
 
   return (
-    <div className={styles.container}>
-      <div className={styles.products}>
-        {!products.length && <Loader />}
-        {products.map((p) => (
-          <Card key={p.id} data={p} />
-        ))}
-        <Loader />
+    <>
+      <div>
+        <input
+          type="text"
+          placeholder="Search..."
+          value={search}
+          onChange={(e) => setSearch(e.target.value.toLocaleLowerCase().trim())}
+        />
+        <button onClick={searchHandler}>
+          <ImSearch />
+        </button>
       </div>
-      <div>sidebar</div>
-    </div>
+      <div className={styles.container}>
+        <div className={styles.products}>
+          {!products.length && <Loader />}
+          {products.map((p) => (
+            <Card key={p.id} data={p} />
+          ))}
+          <Loader />
+        </div>
+        <div>sidebar</div>
+      </div>
+    </>
   );
 }
 
