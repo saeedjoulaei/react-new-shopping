be4b2253df3098aeb5a23f2e45dce42a860f9969
saeed joulaei <saeedjoulaei86@gmail.com>
2024-03-13 01:07:28 +0330

    query management section added

--- src/helper/helper.js
+++ src/helper/helper.js
@@ -2,4 +2,44 @@ const shortenText = (text) => {
   return text.split(" ").slice(0, 3).join("");
 };
 
-export { shortenText };
+const searchProducts = (products, search) => {
+  if (!search) return products;
+  const searchedProducts = products.filter((p) =>
+    p.title.toLowerCase().includes(search)
+  );
+  return searchedProducts;
+};
+const filterProducts = (products, category) => {
+  if (!category) return products;
+  const filterdProducts = products.filter((p) => p.category === category);
+  return filterdProducts;
+};
+
+const createQueryObject = (currentQuery, newQuery) => {
+  if (newQuery.categorie === "all") {
+    const { categorie, ...rest } = currentQuery;
+    return rest;
+  }
+  if (newQuery.search === "") {
+    const { search, ...rest } = currentQuery;
+    return rest;
+  }
+  return { ...currentQuery, ...newQuery };
+};
+const getInitialQuery = (searchParams) => {
+  const query = {};
+  const categorie = searchParams.get("categorie");
+  const search = searchParams.get("search");
+
+  if (categorie) query.categorie = categorie;
+  if (search) query.search = search;
+  return query;
+};
+
+export {
+  shortenText,
+  searchProducts,
+  filterProducts,
+  createQueryObject,
+  getInitialQuery,
+};
--- src/pages/ProductsPages.jsx
+++ src/pages/ProductsPages.jsx
@@ -1,18 +1,51 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { ImSearch } from "react-icons/im";
+import { FaListUl } from "react-icons/fa";
 
 import Card from "../components/Card";
 import Loader from "../components/Loader";
 import { useProducts } from "../context/ProductContext";
 
+import {
+  createQueryObject,
+  filterProducts,
+  getInitialQuery,
+  searchProducts,
+} from "../helper/helper";
 import styles from "../pages/ProductsPages.module.css";
+import { useSearchParams } from "react-router-dom";
 
 function ProductsPages() {
   const products = useProducts();
+  const [displayed, setDisplayed] = useState([]);
   const [search, setSearch] = useState("");
+  const [query, setQuery] = useState({});
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  useEffect(() => {
+    setDisplayed(products);
+
+    setQuery(getInitialQuery(searchParams));
+  }, [products]);
+
+  useEffect(() => {
+    console.log(query);
+    setSearchParams(query);
+    setSearch(query.search || "");
+    let finalProducts = searchProducts(products, query.search);
+    finalProducts = filterProducts(finalProducts, query.categorie);
+    setDisplayed(finalProducts);
+  }, [query]);
 
   const searchHandler = () => {
-    console.log("click");
+    setQuery((query) => createQueryObject(query, { search }));
+  };
+  const categoriHandler = (e) => {
+    const { tagName } = e.target;
+    const categorie = e.target.innerText.toLowerCase();
+    if (tagName !== "LI") return;
+
+    setQuery((query) => createQueryObject(query, { categorie }));
   };
 
   return (
@@ -22,7 +55,7 @@ function ProductsPages() {
           type="text"
           placeholder="Search..."
           value={search}
-          onChange={(e) => setSearch(e.target.value.toLocaleLowerCase().trim())}
+          onChange={(e) => setSearch(e.target.value.toLowerCase().trim())}
         />
         <button onClick={searchHandler}>
           <ImSearch />
@@ -30,13 +63,24 @@ function ProductsPages() {
       </div>
       <div className={styles.container}>
         <div className={styles.products}>
-          {!products.length && <Loader />}
-          {products.map((p) => (
+          {!displayed.length && <Loader />}
+          {displayed.map((p) => (
             <Card key={p.id} data={p} />
           ))}
-          <Loader />
         </div>
-        <div>sidebar</div>
+        <div>
+          <div>
+            <FaListUl />
+            <p>Categories</p>
+          </div>
+          <ul onClick={categoriHandler}>
+            <li>All</li>
+            <li>Electronics</li>
+            <li>Jewelery</li>
+            <li>Men's Clothing</li>
+            <li>Women's Clothing</li>
+          </ul>
+        </div>
       </div>
     </>
   );
